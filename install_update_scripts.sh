7ccd3d6ec5f1a88697f933e67f9cc2fb5493f2df
CuriousTommy <CuriousTommy@users.noreply.github.com>
2016-12-15 14:02:23 -0800

    Create install_update_scripts.sh

---
+++ install_update_scripts.sh
@@ -0,0 +1,7 @@
+#Let start with devkitARM first
+chmod +x ./devkitARMupdate.pl
+./devkitARMupdate.pl /opt/devkitpro
+
+#Then run devkitPPC
+chmod +x ./devkitPPCupdate.pl
+./devkitPPCupdate.pl /opt/devkitpro
