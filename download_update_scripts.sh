8a3db0a4af165aebf51b819225cf9bcfa953c7c3
CuriousTommy <CuriousTommy@users.noreply.github.com>
2016-12-15 13:26:41 -0800

    Update download_update_scripts.sh
    Give it their proper names instead of just having the name download

--- download_update_scripts.sh
+++ download_update_scripts.sh
@@ -1,3 +1,3 @@
 # Downloads the update scripts for devkitARM and devkitPPC
-wget https://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitARMupdate.pl/download
-wget https://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitPPCupdate.pl/download
+wget -O devkitARMupdate.pl 'https://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitARMupdate.pl/download#'
+wget -O devkitPPCupdate.pl 'https://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitPPCupdate.pl/download#'
